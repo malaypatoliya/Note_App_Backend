730344dbcaa0dceacc388d2f4b996b4a1febcf9f
Malay Patoliya <malaypatoliya1619@gmail.com>
2022-06-16 11:53:18 +0530

    checking unique email & validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,35 @@
 const express = require('express');
-const User = require('../models/User')
-
 const router = express.Router();
+const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+
+// create a user using: createUser -- No login required
+router.post('/createUser', [
+
+    body('name', 'Enter valid name').isLength({ min: 3 }),
+    body('email', 'Enter valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
+
+] , async (req, res)=>{
+    // If errors, then return bad request and errors 
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      .then(user => res.json(user))
+      .catch(err => {
+        console.log(err);
+        res.json({error: "Please enter unique email"});
+      });
+
 })
 
 module.exports = router;
