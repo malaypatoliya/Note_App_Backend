8272df552ae15d6c550938f18320345798d4ea89
Malay Patoliya <malaypatoliya1619@gmail.com>
2022-06-16 10:51:31 +0530

    data save commit

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+// connection to mongoDB
 const connectToMongo = require('./db');
 connectToMongo();
 
+// create express server
 const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
 
+// endpoint (router)
+app.use('/', require('./routes/auth'));
+
+// app.get('/', (req, res)=>{
+//     res.send('Hello world !!')
+// })
+
+
+// listening the request on port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
